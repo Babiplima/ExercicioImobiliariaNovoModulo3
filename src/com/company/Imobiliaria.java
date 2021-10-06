12f650f59ca9f3bbe0434f29a6f63219d8f0dbc5
Bárbara Ann Lima <babiplima@gmail.com>
2021-10-06 12:03:59 -0300

    Commit Classe Imobiliaria refatorada

--- src/com/company/Imobiliaria.java
+++ src/com/company/Imobiliaria.java
@@ -5,14 +5,45 @@ import java.util.List;
 
 public class Imobiliaria {
 
+    //Atributos
+
+    private String nomeImobiliaria;
     private List<Imovel> imoveis = new ArrayList<>();
+
+    //Métodos construtores
+
+    public Imobiliaria() {
+    }
+
+    public Imobiliaria(String nomeImobiliaria) {
+        this.nomeImobiliaria = nomeImobiliaria;
+    }
+   //Método Getters e Setters
+
+
+    public String getNomeImobiliaria() {
+        return nomeImobiliaria;
+    }
+
+    public void setNomeImobiliaria(String nomeImobiliaria) {
+        this.nomeImobiliaria = nomeImobiliaria;
+    }
 
     public List<Imovel> getImoveis() {
         return imoveis;
     }
 
-    public void adicionarImovel(Imovel imovel) {
-        imoveis.add(imovel);
+    public void setImoveis(List<Imovel> imoveis) {
+        this.imoveis = imoveis;
+    }
+    //Método toString
+
+    @Override
+    public String toString(){
+        StringBuilder exibir = new StringBuilder();
+        exibir.append("================"+nomeImobiliaria+"==================");
+        exibir.append("=============="+imoveis+"==================");
+        return exibir.toString();
     }
 }
 
