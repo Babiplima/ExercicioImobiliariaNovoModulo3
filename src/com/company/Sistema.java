67e535ebc1945d1d5ec644741f11c1b1956ea393
Bárbara Ann Lima <babiplima@gmail.com>
2021-10-06 12:09:37 -0300

    Criar menu

--- src/com/company/Sistema.java
+++ src/com/company/Sistema.java
@@ -12,6 +12,26 @@ public class Sistema {
         System.out.println(mensagem);
         return new Scanner(System.in);
     }
+    //Menu inicial
+    public static int menuInicial() {
+        System.out.println("Seja muito bem vindo ao sistema imobiliário");
+        System.out.println("Por favor, digite 1 para cadastrar um novo imóvel ");
+        System.out.println("Por favor, digite 2 para exibir os imóveis ");
+        System.out.println("Por favor, digite 3 para excluir um morador ");
+        System.out.println("Por favor 4 , caso deseje sair digite 4 ");
+        return menuInicial();
+    }
 
+    //Entrada de dados
+
+    public static Moradores novoMorador() {
+        String nome = capturarDados("Digite o nome do morador ").nextLine();
+        String cpf = capturarDados("Digite o seu CPF").nextLine();
+        String telefone = capturarDados("Digite o seu telefone ").nextLine();
+        String endereço = capturarDados("Digite o endereço").nextLine();
+        String aluguel = capturarDados("Digite o valor do aluguel").nextLine();
+        Moradores moradores = new Moradores();
+        return novoMorador();
+    }
 
 }
