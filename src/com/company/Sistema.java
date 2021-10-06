47e32c34c4f822081fab4e222a9f5a670ff7d6af
Bárbara Ann Lima <babiplima@gmail.com>
2021-10-06 12:06:54 -0300

    Commit método capturar dados

--- src/com/company/Sistema.java
+++ src/com/company/Sistema.java
@@ -1,11 +1,17 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Scanner;
 
 
 public class Sistema {
+
+    //Método leitor de dados
+
+    private static Scanner capturarDados(String mensagem) {
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
 
 
 }
